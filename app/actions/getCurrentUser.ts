99dae33928faf24b9149d8e2b21d0a98efc8f997
julia-rodrigo <juliarodrigo2301@gmail.com>
2023-07-11 02:11:54 +0100

    git and google sessions, but errrors when deleted user manually from database

--- app/actions/getCurrentUser.ts
+++ app/actions/getCurrentUser.ts
@@ -25,9 +25,15 @@ export default async function getCurrentUser () {
             return null;
         }
 
-        return currentUser;
+        return {
+            ...currentUser,
+            createdAt: currentUser.createdAt.toISOString(),
+            updatedAt: currentUser.updatedAt.toISOString(),
+            emailVerified: currentUser.emailVerified?.toISOString() || null,
+        }
 
     } catch (error: any) {
+        console.log("error here")
         return null;
     }
 }
