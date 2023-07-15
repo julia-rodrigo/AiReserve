01d1fd07d20b2bc2021f7a44819964d527133182
julia-rodrigo <juliarodrigo2301@gmail.com>
2023-07-15 22:31:19 +0100

    place location and marker

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -7,6 +7,8 @@ import Heading from "../general/Heading";
 import { categories } from "../navbar/Categories";
 import CategoryInput from "../inputs/CategoryInput";
 import { FieldValues, useForm } from "react-hook-form";
+import CountrySelect from "../inputs/CountrySelect";
+import dynamic from "next/dynamic";
 
 enum STEPS {
     CATEGORY = 0,
@@ -49,6 +51,11 @@ const RentModal = () => {
     })
 
     const category = watch('category');
+    const location = watch('location');
+
+    const Map = useMemo(() => dynamic(() => import('../general/Map'), {
+        ssr: false
+    }), [location])
 
     const setCustomValue = (id: string, value: any) => {
         setValue(id, value, {
@@ -78,6 +85,8 @@ const RentModal = () => {
         return 'Back';
     }, [step])
 
+    
+
     let bodyContent = (
         <div className="flex flex-col gap-8">
             <Heading 
@@ -105,13 +114,31 @@ const RentModal = () => {
         </div>
     )
 
+    if ( step == STEPS.LOCATION) {
+        bodyContent = (
+            <div className="flex flex-col gap-8">
+                <Heading
+                    title="Where is your place located?"
+                    subtitle="Help guests find you!"
+                />
+                <CountrySelect
+                    value={location}
+                    onChange={(value) => setCustomValue('location', value)}
+                />
+                <Map 
+                    center={location?.latlng}
+                />
+            </div>
+        )
+    }
+
 
 
   return (
     <Modal
         isOpen={rentModal.isOpen}
         onClose={rentModal.onClose}
-        onSubmit={rentModal.onClose}
+        onSubmit={onNext}
         actionLabel={actionLabel}
         secondaryActionLabel={secondaryActionLabel}
         secondaryAction={ step == STEPS.CATEGORY ? undefined : onBack }
