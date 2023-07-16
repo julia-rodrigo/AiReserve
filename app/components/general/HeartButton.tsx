97bd73326ade7bada32280950b9c9cc0ed7990e9
julia-rodrigo <juliarodrigo2301@gmail.com>
2023-07-17 00:43:22 +0100

    favourite listing added

--- app/components/general/HeartButton.tsx
+++ app/components/general/HeartButton.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import useFavorite from "@/app/hooks/useFavorite";
 import { SafeUser } from "@/app/types";
 import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
 
@@ -12,12 +13,14 @@ const HeartButton: React.FC<HeartButtonProps> = ({
     listingId,
     currentUser
 }) => {
-    const hasFavourited = false;
-    const toggleFavourite = () => {};
+    const { hasFavorited, toggleFavorite } = useFavorite({
+        listingId,
+        currentUser
+    });
 
   return (
     <div
-        onClick={toggleFavourite}
+        onClick={toggleFavorite}
         className="relative hover:opacity-80 transition cursor-pointer"
     >
         <AiOutlineHeart 
@@ -27,7 +30,7 @@ const HeartButton: React.FC<HeartButtonProps> = ({
         <AiFillHeart 
             size={24}
             className={
-                hasFavourited ? 'fill-rose-500' : 'fill-neutral-500/70'
+                hasFavorited ? 'fill-rose-500' : 'fill-neutral-500/70'
             }
         />
     </div>
