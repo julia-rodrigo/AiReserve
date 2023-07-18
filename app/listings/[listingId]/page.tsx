3a89a12984ee79ba6c4afae9e14fef9414fab48f
julia-rodrigo <juliarodrigo2301@gmail.com>
2023-07-18 18:32:31 +0100

    booking reservations actions completed

--- app/listings/[listingId]/page.tsx
+++ app/listings/[listingId]/page.tsx
@@ -3,6 +3,7 @@ import getListingById from "@/app/actions/getListingById"
 import ClientOnly from "@/app/components/general/ClientOnly";
 import EmptyState from "@/app/components/general/EmptyState";
 import ListingClient from "./ListingClient";
+import getReservations from "@/app/actions/getReservations";
 
 interface IParams {
     listingId?: string;
@@ -10,6 +11,7 @@ interface IParams {
 
 const listingPage = async ({ params }: { params: IParams }) => {
     const listing = await getListingById(params);
+    const reservations = await getReservations(params);
     const currentUser = await getCurrentUser();
 
     if (!listing) {
@@ -25,6 +27,7 @@ const listingPage = async ({ params }: { params: IParams }) => {
         <ListingClient
             listing={listing}
             currentUser={currentUser}
+            reservations={reservations}
         />
     </ClientOnly>
   )
